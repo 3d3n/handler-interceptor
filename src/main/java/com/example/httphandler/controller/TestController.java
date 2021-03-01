6b91ff354d87f5392b3d065eec420341d821bd60
eajibade <eajibade@ecobank.com>
2021-03-01 17:13:18 +0100

    add test method formats

--- src/main/java/com/example/httphandler/controller/TestController.java
+++ src/main/java/com/example/httphandler/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.example.httphandler.controller;
 
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,20 @@ public class TestController {
     public ResponseEntity<?> test(@RequestParam("name") String name) {
 
         return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
+
+    }
+
+    @PostMapping("json")
+    public ResponseEntity<?> test2(@RequestBody String request) {
+
+        System.out.println(request);
+
+        JSONObject jsonObject = new JSONObject(request);
+        jsonObject.put("status", "Done");
+
+        System.out.println(jsonObject);
+
+        return new ResponseEntity<>(jsonObject, HttpStatus.OK);
 
     }
 }
--- src/test/java/com/example/httphandler/HttphandlerApplicationTests.java
+++ src/test/java/com/example/httphandler/HttphandlerApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.httphandler;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -19,7 +20,7 @@ class HttphandlerApplicationTests extends AbstractTest{
 
 
 	@Test
-	public void createProduct() throws Exception {
+	public void test() throws Exception {
 		try {
 			String uri = "/hello";
 			String firstName = "foo";
@@ -39,5 +40,86 @@ class HttphandlerApplicationTests extends AbstractTest{
 			e.printStackTrace();
 		}
 	}
+
+	@Test
+	public void test2() throws Exception {
+		try {
+			String uri = "/json";
+			String firstName = "foo";
+			MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders
+					.post(uri)
+					.header("TOKEN", "YES")
+//					.param("name", firstName))
+					.contentType(MediaType.APPLICATION_JSON))
+					.andReturn();
+
+			int status = mvcResult.getResponse().getStatus();
+			System.out.println("status: " + status);
+			assertEquals(200, status);
+			String content = mvcResult.getResponse().getContentAsString();
+			System.out.println("content: " + content);
+			assertEquals(content, "Hello " + firstName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+//	@Test //GET
+//	public void getProductsList() throws Exception {
+//		String uri = "/products";
+//		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+//				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+//
+//		int status = mvcResult.getResponse().getStatus();
+//		assertEquals(200, status);
+//		String content = mvcResult.getResponse().getContentAsString();
+//		Product[] productlist = super.mapFromJson(content, Product[].class);
+//		assertTrue(productlist.length > 0);
+//	}
+//
+//	@Test //POST
+//	public void createProduct() throws Exception {
+//		String uri = "/products";
+//		Product product = new Product();
+//		product.setId("3");
+//		product.setName("Ginger");
+//		String inputJson = super.mapToJson(product);
+//		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+//				.contentType(MediaType.APPLICATION_JSON_VALUE)
+//				.content(inputJson)).andReturn();
+//
+//		int status = mvcResult.getResponse().getStatus();
+//		assertEquals(201, status);
+//		String content = mvcResult.getResponse().getContentAsString();
+//		assertEquals(content, "Product is created successfully");
+//	}
+//
+//	@Test //PUT
+//	public void updateProduct() throws Exception {
+//		String uri = "/products/2";
+//		Product product = new Product();
+//		product.setName("Lemon");
+//		String inputJson = super.mapToJson(product);
+//		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
+//				.contentType(MediaType.APPLICATION_JSON_VALUE)
+//				.content(inputJson)).andReturn();
+//
+//		int status = mvcResult.getResponse().getStatus();
+//		assertEquals(200, status);
+//		String content = mvcResult.getResponse().getContentAsString();
+//		assertEquals(content, "Product is updated successsfully");
+//	}
+//
+//	@Test //DELETE
+//	public void deleteProduct() throws Exception {
+//		String uri = "/products/2";
+//		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+//		int status = mvcResult.getResponse().getStatus();
+//		assertEquals(200, status);
+//		String content = mvcResult.getResponse().getContentAsString();
+//		assertEquals(content, "Product is deleted successsfully");
+//	}
 
 }
